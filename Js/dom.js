69a079af9268b51fd250fbc951e525dc5790d830
maruf-hussain <engmarufhussain@gmail.com>
2023-06-26 14:13:20 +0600

    lsjjgfoawiufoii

--- Js/dom.js
+++ Js/dom.js
@@ -1,2 +1,61 @@
-console.log('Hello JavaScript');
-console.log(document.body);
+// const brandName = document.getElementById('brand-name');
+// brandName.style.color = 'red';
+// brandName.style.backgroundColor = 'yellow';
+
+// const brand = document.getElementsByClassName('Brand');
+// brand.style.color = 'red';
+// brand.style.border = '3px solid blue';
+// brand.style.borderRadius = '10 px'
+
+
+// const homeList = document.getElementById('home');
+// homeList.classList.add('color');
+// homeList.classList.add('background-color');
+
+
+const mainContainer = document.getElementById('main-container');
+
+// const section = document.createElement('section');
+// mainContainer.appendChild(section);
+
+// const h1 = document.createElement('h1');
+// h1.innerText = 'My Favorite Food';
+// section.appendChild(h1);
+// section.appendChild(ul);
+
+// const ul = document.createElement(ul);
+
+
+// const li1 = document.createElement('li');
+// li1.innerText = 'biriyani';
+
+// ul.appendChild(li1);
+
+
+// const li2 = document.createElement('li');
+// li2.innerText = 'Nodools';
+
+// ul.appendChild(li2);
+
+// const li3 = document.createElement('li');
+// li3.innerText = 'egg vaji';
+
+// ul.appendChild(li3);
+
+
+const secttionDess = document.createElement('section');
+secttionDess.innerHTML = `
+<h1>My Food Is Good</h1>
+<ul>
+<li>ami</li>
+<li>tumi</li>
+<li>amra</li>
+
+</ul>
+`
+mainContainer.appendChild(secttionDess);
+
+
+
+
+
