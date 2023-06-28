f4f120f94d0b3f44c1af7bfa695c8019981a00ac
maruf-hussain <engmarufhussain@gmail.com>
2023-06-29 02:15:50 +0600

    jflawwjoi

--- Bank/banking.js
+++ Bank/banking.js
@@ -1,5 +1,18 @@
 document.getElementById('deposit-button').addEventListener('click', function(){
-console.log('deposit button clicked')
+const deposit = document.getElementById('deposit-amount');
+const depositText = deposit.value;
+const newDepositText = parseFloat(depositText);
+
+const depositTotal = document.getElementById('deposit');
+const depositTotalText = depositTotal.innerText;
+const newDepositTotalText = parseFloat(depositTotalText);
+
+const currentDepositTotal = newDepositText + newDepositTotalText;
+depositTotal.innerText = currentDepositTotal;
+
+deposit.value = '';
+
+
 })
 
 // .............update balance.................//
