d00678f0e1ca09478aa0c9ed9d21c5b310ec9b88
maruf-hussain <engmarufhussain@gmail.com>
2023-07-02 00:52:07 +0600

    faljgla

--- Bank/functonal/withdraw.js
+++ Bank/functonal/withdraw.js
@@ -1,1 +1,10 @@
-console.log('it connected')
+document.getElementById('withdraw-button').addEventListener('click', function(){
+  const withdrawAmount = getInputValueById('withdraw-amount');
+  const withdrawTotal = getElementById('withdraw');
+  const newWithdrawTotal = withdrawAmount + withdrawTotal;
+  setTotalDeposit('withdraw', newWithdrawTotal);
+  const balanceTotal = getElementById('balance');
+  const balanceAfterWithdraw = balanceTotal - withdrawAmount;
+  setTotalDeposit('balance', balanceAfterWithdraw);
+
+})
